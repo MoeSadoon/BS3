118ddd6e0209f88f8b88663fb38c5cb1297fcd0e
Proskurina <yproskurina@yahoo.com>
2015-09-10 22:49:14 +0100

    Refactored ship tests

--- spec/ship_spec.rb
+++ spec/ship_spec.rb
@@ -2,28 +2,24 @@ require 'ship'
 
 describe Ship do
 
+  before :each do
+    @ship = Ship.new('A1', :E)
+  end
+
   it "increases number of hits when hit" do
-    board = Board.new
-    ship = Ship.new('A1', :N)
-    hit_num = ship.hits
-    board.place ship
-    board.receive_a_hit('A1')
-    expect(ship.hits).to eq(hit_num+1)
+    hit_num = @ship.hits
+    @ship.hit('A1')
+    expect(@ship.hits).to eq(hit_num+1)
   end
 
   it "is not sunk when created" do
-    ship = Ship.new('A1', :E)
-    expect(ship).not_to be_sunk
+    expect(@ship).not_to be_sunk
   end
 
   it "is sunk when it gets hit" do
-    ship = Ship.new('A1', :E)
-    ship.was_hit
-    expect(ship).to be_sunk
+    @ship.was_hit
+    expect(@ship).to be_sunk
   end
 
 end
 
-# Version 'Skateboard'
-# describe Ship do
-# end
